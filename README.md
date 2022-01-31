7d415f37e7d46e6238875559be5bdc649833eb5b
Leonardo Russo <info@lrusso.com>
2022-01-30 22:24:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# PixelGym
-PixelGym
+# Pixel Gym
+
+Do aerobic exercises using you webcam.
+
+![alt screenshot](PixelGym.png?raw=true)
+
+## Web
+
+https://www.pixelgym.com
+
+## Debug mode
+
+https://www.pixelgym.com/?debug=true
+
+## Demo using the Debug mode 
+
+![alt demo](PixelGym.gif?raw=true)
+
+## Based on the work of:
+
+https://github.com/jasonmayes/JS-Motion-Detection
+
+<!--
+
+ffmpeg -i in.mp4 -filter:v "crop=1080:1889:0:260" -c:a copy out.mp4
+ffmpeg -i in.mp4 -filter:v "crop=720:1250:0:180" -c:a copy out.mp4
+ffmpeg -i in.mp4 -filter:v "crop=675:1250:25:180" -c:a copy out.mp4 
+ffmpeg -i in.mp4 -filter:v "crop=650:1250:35:180" -c:a copy out.mp4 
+
+-->
---
+++ worker.js
@@ -0,0 +1,35 @@
+const filesToCache = [
+	"PixelGym.htm",
+	"PixelGym.json",
+	"PixelGym.png",
+	"PixelGymFavIcon_16x16.png",
+	"PixelGymFavIcon_192x192.png",
+	"PixelGymFavIcon_512x512.png",
+	"PixelGymGame.htm",
+	"PixelGymGame.js",
+	"PixelGymShare.png"
+];
+
+const staticCacheName = "pixelgym-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
